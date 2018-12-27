bd732a9f2745a1e68e9964037cf36d60dd8b541a
chengshunzhang <chengshun.zhang@outlook.com>
2018-12-26 16:19:18 -0800

    MatchAllFilter and modification to the interface

--- Java_Programming_Principles_of_Software_Design/EarthquakeFilterStarterProgram/EarthQuakeClient2.java
+++ Java_Programming_Principles_of_Software_Design/EarthquakeFilterStarterProgram/EarthQuakeClient2.java
@@ -35,9 +35,45 @@ public class EarthQuakeClient2 {
         ArrayList<QuakeEntry> qpd = filter(qd, fPhr);
         for (QuakeEntry qe: qpd) { 
             System.out.println(qe);
-        } 
+        }
     }
 
+    public void testMatchAllFilter() {
+        EarthQuakeParser parser = new EarthQuakeParser(); 
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        String source = "data/nov20quakedatasmall.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);         
+        System.out.println("read data for "+list.size()+" quakes");
+        
+        MatchAllFilter maf = new MatchAllFilter();
+        maf.addFilter(new MagnitudeFilter(0.0, 2.0));
+        maf.addFilter(new DepthFilter(-100000.0, -10000.0));
+        maf.addFilter(new PhraseFilter("any", "a"));
+        ArrayList<QuakeEntry> quakes = filter(list, maf);
+        for (QuakeEntry qe: quakes) { 
+            System.out.println(qe);
+        }
+        System.out.println("Filters used are: " + maf.getName());
+    }
+    
+    public void testMatchAllFilter2() {
+        EarthQuakeParser parser = new EarthQuakeParser(); 
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        String source = "data/nov20quakedatasmall.atom";
+        ArrayList<QuakeEntry> list  = parser.read(source);         
+        System.out.println("read data for "+list.size()+" quakes");
+        
+        MatchAllFilter maf = new MatchAllFilter();
+        maf.addFilter(new MagnitudeFilter(0.0, 3.0));
+        maf.addFilter(new DistanceFilter(new Location(36.1314, -95.9372), 10000000));
+        maf.addFilter(new PhraseFilter("any", "Ca"));
+        ArrayList<QuakeEntry> quakes = filter(list, maf);
+        for (QuakeEntry qe: quakes) { 
+            System.out.println(qe);
+        }
+        System.out.println("Filters used are: " + maf.getName());
+    }
+    
     public void createCSV() {
         EarthQuakeParser parser = new EarthQuakeParser();
         //String source = "../data/nov20quakedata.atom";
